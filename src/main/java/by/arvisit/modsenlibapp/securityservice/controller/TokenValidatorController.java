65f4cefc2f425a2f087135f2156875dceade4551
Artem Sitko <sitko.ar@gmail.com>
2024-02-10 23:33:57 +0300

    Add custom exceptions

--- src/main/java/by/arvisit/modsenlibapp/securityservice/controller/TokenValidatorController.java
+++ src/main/java/by/arvisit/modsenlibapp/securityservice/controller/TokenValidatorController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import by.arvisit.modsenlibapp.exceptionhandlingstarter.exception.BadRequestException;
+import by.arvisit.modsenlibapp.exceptionhandlingstarter.exception.InvalidTokenException;
 import by.arvisit.modsenlibapp.securityservice.dto.UserDto;
 import by.arvisit.modsenlibapp.securityservice.service.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +22,12 @@ public class TokenValidatorController {
     @GetMapping("/validate")
     public UserDto validate(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authHeader) {
         if (!authHeader.startsWith("Bearer ")) {
-            throw new RuntimeException("Bad header"); // TODO bad practice, correct!
+            throw new BadRequestException("Invalid authentication header");
         }
 
         final String token = authHeader.split(" ")[1].trim();
         if (!jwtService.isValid(token)) {
-            throw new RuntimeException("Bad token");// TODO bad practice, correct!
+            throw new InvalidTokenException("Invalid token");
         }
         return jwtService.extractUser(token);
     }
